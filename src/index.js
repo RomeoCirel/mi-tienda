e6ad32ad7f28490893cf88c5ec032be58b807288
Cirel Roimeo <cirelhromeo@gmail.com>
2022-08-25 12:23:39 -0300

    Rework: agrega el Nav & define las rutas en index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes  } from "react-router-dom";
 import './index.css';
-import App from './App';
+import Nav from './components/Nav'
+import Home from './pages/Home'
+import MisCompras from './pages/MisCompras'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Nav />
+    <Routes>
+      <Route exact path='/' element={<Home />} />
+      <Route exact path='/mis-compras'  element={<MisCompras/>} />
+    </Routes>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
