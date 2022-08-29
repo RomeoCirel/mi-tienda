844e407495e270d0b62e035d18e285c07b7b2cf2
Cirel Roimeo <cirelhromeo@gmail.com>
2022-08-29 14:24:33 -0300

    Fix: RouteContext inicia su estado en null

--- src/contexts/RouteContext.jsx
+++ src/contexts/RouteContext.jsx
@@ -1,12 +1,11 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
-/* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/prop-types */
 import { createContext, useState } from "react";
 
 export const RouteContext = createContext();
 
 export const RouteProvider = ({ children }) => {
-  const [sectionName, setSectionName] = useState("seccioncontext");
+  const [sectionName, setSectionName] = useState(null);
   return (
     <RouteContext.Provider value={[sectionName, setSectionName]}>
       {children}
