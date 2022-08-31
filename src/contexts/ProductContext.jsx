f72792695dacb21329ec75c672b5da0930d365bc
Cirel Roimeo <cirelhromeo@gmail.com>
2022-08-31 15:58:32 -0300

    Fix: ProductContext product.price a numero y agrega product.stock

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -8,7 +8,8 @@ export const ProductProvider = ({ children }) => {
     {
       id: 100,
       name: "Motherboard Gigabytes x570 Aorus Ultra",
-      price: "67.999",
+      price: 67999,
+      stock: 10,
       image:
         "https://http2.mlstatic.com/D_NQ_NP_678028-MLA46275878652_062021-O.webp",
       inCart: false,
@@ -20,7 +21,8 @@ export const ProductProvider = ({ children }) => {
     {
       id: 101,
       name: "Placa De Video Zotac Geforce Rtx 3070 Twin Edge Lhr 8 Gb",
-      price: "139.999",
+      price: 139999,
+      stock: 10,
       image:
         "https://http2.mlstatic.com/D_NQ_NP_758476-MLA49500182252_032022-O.webp",
       inCart: false,
@@ -32,7 +34,8 @@ export const ProductProvider = ({ children }) => {
     {
       id: 102,
       name: "Disco sólido interno Gigabyte Aorus 2TB",
-      price: "72.834",
+      price: 72834,
+      stock: 10,
       image:
         "https://http2.mlstatic.com/D_NQ_NP_858090-MLA43220897090_082020-O.webp",
       inCart: false,
@@ -44,7 +47,8 @@ export const ProductProvider = ({ children }) => {
     {
       id: 103,
       name: "Notebook Hp New Elite I7 11va 32gb 17.3 1tb Ssd 16gb Video",
-      price: "479.999",
+      price: 479999,
+      stock: 10,
       image:
         "https://http2.mlstatic.com/D_NQ_NP_698455-MLA49976265495_052022-O.webp",
       inCart: false,
@@ -56,7 +60,8 @@ export const ProductProvider = ({ children }) => {
     {
       id: 104,
       name: "Micrófono Blue Yeti condensador multipatrón blackout",
-      price: "24.499",
+      price: 24499,
+      stock: 10,
       image:
         "https://http2.mlstatic.com/D_NQ_NP_926212-MLA50186554835_062022-O.webp",
       inCart: false,
@@ -68,7 +73,8 @@ export const ProductProvider = ({ children }) => {
     {
       id: 105,
       name: "Apple Macbook Air (13 pulgadas, 2020, Chip M1, 256 GB de SSD, 8 GB de RAM)",
-      price: "249.990",
+      price: 249990,
+      stock: 10,
       image:
         "https://http2.mlstatic.com/D_NQ_NP_801112-MLA46516512347_062021-O.webp",
       inCart: false,
@@ -80,7 +86,8 @@ export const ProductProvider = ({ children }) => {
     {
       id: 106,
       name: 'Apple iPad Pro de 12.9" Wi-Fi 256GB Gris espacial (5ª generación)',
-      price: "349.999",
+      price: 349999,
+      stock: 10,
       image:
         "https://http2.mlstatic.com/D_NQ_NP_922043-MLA46975166576_082021-O.webp",
       inCart: false,
@@ -92,7 +99,8 @@ export const ProductProvider = ({ children }) => {
     {
       id: 107,
       name: "Router TP-Link Archer C80 V1 negro",
-      price: "10.899",
+      price: 10899,
+      stock: 10,
       image:
         "https://http2.mlstatic.com/D_NQ_NP_628885-MLA42822340742_072020-O.webp",
       inCart: false,
@@ -104,7 +112,8 @@ export const ProductProvider = ({ children }) => {
     {
       id: 108,
       name: "Memoria RAM Vengeance RGB Pro gamer color negro 16GB 2 Corsair",
-      price: "20.399",
+      price: 20399,
+      stock: 10,
       image:
         "https://http2.mlstatic.com/D_NQ_NP_684969-MLA47962858193_102021-O.webp",
       inCart: false,
@@ -116,7 +125,8 @@ export const ProductProvider = ({ children }) => {
     {
       id: 109,
       name: "Amd Ryzen 9 5950x Am4 4.9 Ghz",
-      price: "186.499",
+      price: 186499,
+      stock: 10,
       image:
         "https://http2.mlstatic.com/D_NQ_NP_957431-MLA47177627550_082021-O.webp",
       inCart: false,
@@ -128,7 +138,8 @@ export const ProductProvider = ({ children }) => {
     {
       id: 110,
       name: "Samsung 870 EVO MZ-77E1T0BW 1TB negro",
-      price: "38.999",
+      price: 38999,
+      stock: 10,
       image:
         "https://http2.mlstatic.com/D_NQ_NP_839137-MLA46735174616_072021-O.webp",
       inCart: false,
@@ -140,7 +151,8 @@ export const ProductProvider = ({ children }) => {
     {
       id: 111,
       name: "Intel Core I9 12900k 5.2 Ghz Alder 1700",
-      price: "164.719",
+      price: 164719,
+      stock: 10,
       image:
         "https://http2.mlstatic.com/D_NQ_NP_936329-MLA48255412231_112021-O.webp",
       inCart: false,
@@ -152,7 +164,8 @@ export const ProductProvider = ({ children }) => {
     {
       id: 112,
       name: "Asus Rog Strix X570-e Gaming Atx Motherboard With Pcie 4",
-      price: "265.029",
+      price: 265029,
+      stock: 10,
       image:
         "https://http2.mlstatic.com/D_NQ_NP_840338-MLA32448766651_102019-O.webp",
       inCart: false,
@@ -164,7 +177,8 @@ export const ProductProvider = ({ children }) => {
     {
       id: 113,
       name: "Gabinete Pc Case Thermaltake View 71 Tg Argb - Full Tower",
-      price: "80.000",
+      price: 80000,
+      stock: 10,
       image:
         "https://http2.mlstatic.com/D_NQ_NP_2X_883140-MLA50290561723_062022-F.webp",
       inCart: false,
